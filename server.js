bd081d72e482d15b74c02b3fee9cab4fa4a4a98c
Sean Parker <seanrprkr@gmail.com>
2026-06-24 16:09:31 -0400

    users api, 404, and about pages

--- server.js
+++ server.js
@@ -11,3 +11,21 @@ app.listen(3000, () =>  {
   console.log("Listening on http://localhost:3000")
 })
 
+
+app.get("/about", (req, res) => {
+  res.send("About page")
+});
+
+
+// users api
+app.get("/api/users", (req, res) => {
+  res.json([
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" }
+  ])
+});
+
+// Handle unknown routes
+app.use((req, res) => {
+  res.status(404).send("Sorry, page not found")
+});
